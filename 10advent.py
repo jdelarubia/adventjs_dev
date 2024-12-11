b6f18e617a5beb6236d3654688d3c9d54906721a
jdelarubia <jdelarubia@gmail.com>
2024-12-11 17:16:39 +0100

    challenge 10

---
+++ 10advent.py
@@ -0,0 +1,94 @@
+"""10advent.py
+The elf programmers are creating a small magical assembler to control the machines in Santa Claus's workshop.
+
+To assist them, we will implement a simple interpreter that supports the following magical instructions:
+
+    MOV x y: Copies the value x (which can be a number or the content of a register) into register y
+    INC x: Increments the content of register x by 1
+    DEC x: Decrements the content of register x by 1
+    JMP x y: If the value in register x is 0, then jumps to the instruction at index y and continues executing the program from there.
+
+Expected behavior:
+
+    If an attempt is made to access, increment, or decrement a register that has not been initialized, the default value 0 will be used.
+    The jump with JMP is absolute and goes to the exact index indicated by y.
+    Upon completion, the program should return the content of register A. If A did not have a defined value, it returns undefined.
+
+const instructions = [
+  'MOV -1 C', // copies -1 to register 'C',
+  'INC C', // increments the value of register 'C'
+  'JMP C 1', // jumps to the instruction at index 1 if 'C' is 0
+  'MOV C A', // copies register 'C' to register 'A',
+  'INC A' // increments the value of register 'A'
+]
+
+compile(instructions) // -> 2
+
+/**
+ Step-by-step execution:
+ 0: MOV -1 C -> The register C receives the value -1
+ 1: INC C    -> The register C becomes 0
+ 2: JMP C 1  -> C is 0, jumps to the instruction at index 1
+ 1: INC C    -> The register C becomes 1
+ 2: JMP C 1  -> C is 1, the instruction is ignored
+ 3: MOV C A  -> Copies register C to A. Now A is 1
+ 4: INC A    -> The register A becomes 2
+ */
+
+Note: Registers that have not been previously initialized are initialized to 0."""
+
+import unittest
+
+instructions = [
+    "MOV -1 C",  # copies -1 to register 'C',
+    "INC C",  # increments the value of register 'C'
+    "JMP C 1",  # jumps to the instruction at index 1 if 'C' is 0
+    "MOV C A",  # copies register 'C' to register 'A',
+    "INC A",  # increments the value of register 'A'
+]
+test_cases = [(instructions, 2)]
+
+
+class ExampleTestCases(unittest.TestCase):
+    def test_return_type_is_bool(self):
+        for test_case in test_cases:
+            case, _ = test_case
+            self.assertIsInstance(compile(case), int)
+
+    def test_return_value_matches_expected(self):
+        for test_case in test_cases:
+            case, expected = test_case
+            self.assertEqual(compile(case), expected)
+
+
+def compile(instructions):
+    registers = {}
+    n_instructions = len(instructions)
+    i = 0
+    while i < n_instructions:  # for idx, instruction in enumerate(instructions):
+        command, *operands = instructions[i].split()
+        dest = operands[-1]
+        if command == "MOV":
+            source = operands[0]
+            try:
+                value = int(source)
+            except ValueError:
+                value = registers.get(source, 0)
+            registers[dest] = value
+        elif command == "INC":
+            registers[dest] = registers.get(dest, 0) + 1
+        elif command == "DEC":
+            registers[dest] = registers.get(dest, 0) - 1
+        elif command == "JMP":
+            source, value = operands
+            if registers.get(source, 0) == 0:
+                i = int(value)
+                continue
+        i += 1
+    return registers.get("A", None)
+
+
+if __name__ == "__main__":
+    print(compile(instructions))
+
+    unittest.main(verbosity=2)
